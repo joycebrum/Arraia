7c0e95650208497d6a46c0d4c39b6530639ce715
joycebrum <joycebrumu.u@gmail.com>
2017-07-09 16:21:23 -0300

    Iniciozinho da cidade
    Coloquei o chão como prefab, quando tiver a sprint vai ser mais facil de
    mudar e o player meio que ja se move so que não colide

---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour {
+    private Rigidbody2D rigidbody;
+    private bool moving;
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Moviment();
+    }
+ 
+    private void Moviment()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector2 moviment = new Vector2(x, y);
+        if(moviment!=Vector2.zero)
+        {
+            moving = true;
+        }
+        else
+        {
+            moving = false;
+        }
+        rigidbody.MovePosition(rigidbody.position + moviment * Time.deltaTime);
+    }
+}
